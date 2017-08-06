a3ca39a767a7fc3d8dd65cc03d31ac1a7d3a1065
leol <ducleoliao@gmail.com>
2017-08-05 18:49:02 -0700

    Starting template inheritance lessons

--- src/users/views.py
+++ src/users/views.py
@@ -5,11 +5,20 @@ from django.shortcuts import render
 # Create your views here.
 # function based view
 def home(request):
-    num = random.randint(0, 100000)
-    some_list = [num, random.randint(0, 100000), random.randint(0, 100000)]
+    num = None
+
+    some_list = [
+        random.randint(0, 100000),
+        random.randint(0, 100000),
+        random.randint(0, 100000)
+        ]
+
+    condition_bool_item = False
+
+        if condition_bool_item:
+            num = random.randint(0, 100000)
 
     context = {
-        "bool_item": False,
         "num": num,
         "some_list": some_list
     }
